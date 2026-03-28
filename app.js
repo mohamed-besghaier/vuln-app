9efca7073cd9156f48f33502501eaa05e07d0920
Besghaier <besghaier.mohamed.04@gmail.com>
2026-03-28 09:37:40 +0100

    add basic routes

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 // app.js
-const express = require("express");
+
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("It works");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/admin.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/dashboard.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/profile.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/upload.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1> 404 - Page Not Found </h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
